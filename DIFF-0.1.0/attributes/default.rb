7e546e47791cbf936d45aae5d863a930c84bf426
dzmitrok <dzmitrok@hotmail.com>
2016-03-28 22:35:11 +0300

    renamed folder

--- DIFF-0.1.0/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['DIFF']['SourceFolder'] = '/r02/helloworld'
-default['DIFF']['DestinFolder'] = '/r02/helloworld_Modified'
--- DIFF-0.1.0/libraries/Diff_helper.rb
+++
@@ -1,9 +0,0 @@
-# module Demo
-#   module Helper
-#     include Chef::Mixin::ShellOut
-#     def are_different?
-#       cmd = shell_out!("diff -r -I '^#.*' #{diff['SourceFolder']} #{diff['DestinFolder']}", {:returns => [0,2]})
-#       #cmd.stderr.empty? && (cmd.stdout =~ /^bacon/)
-#     end
-#   end
-# end
