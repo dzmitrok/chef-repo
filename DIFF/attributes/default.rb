ac67181cf0e4fe4699f8ead62bbdee0fbae74809
dzmitrok <dzmitrok@hotmail.com>
2016-03-29 20:00:06 +0300

    local changes commits

---
+++ DIFF/attributes/default.rb
@@ -0,0 +1,2 @@
+default['DIFF']['SourceFolder'] = '/r02/helloworld'
+default['DIFF']['DestinFolder'] = '/r02/helloworld_Modified'
---
+++ DIFF/libraries/Diff_helper.rb
@@ -0,0 +1,9 @@
+# module Demo
+#   module Helper
+#     include Chef::Mixin::ShellOut
+#     def are_different?
+#       cmd = shell_out!("diff -r -I '^#.*' #{diff['SourceFolder']} #{diff['DestinFolder']}", {:returns => [0,2]})
+#       #cmd.stderr.empty? && (cmd.stdout =~ /^bacon/)
+#     end
+#   end
+# end
