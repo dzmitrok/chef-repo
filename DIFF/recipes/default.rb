63002d4aaeec45093e5d614d2470d02aadbc6d5a
dzmitrok <dzmitrok@hotmail.com>
2016-04-01 23:14:27 +0300

    fixed all end's

--- DIFF/recipes/default.rb
+++ DIFF/recipes/default.rb
@@ -1,45 +1,37 @@
 # Cookbook Name:: DIFF
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, EPAM
 #
-# All rights reserved - Do Not Redistribute
+# NO rights reserved - Do Not Redistribute
 #
 diff = node['DIFF']
-#  bash 'compare' do
-#    code <<-EOH
-#      #compare 2 folders using linux command diff
-#      diff -r -I '^#.*' #{diff['SourceFolder']} #{diff['DestinFolder']}
-#    EOH
-# end
 ruby_block "check_diff_command_output" do
     block do
       #tricky way to load this Chef::Mixin::ShellOut utilities
       Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-      cmd = "diff -r -I -q '^#.*' #{diff['SourceFolder']} #{diff['DestinFolder']}"
+      cmd = "diff -q -r -I '^#.*' #{diff['SourceFolder']} #{diff['DestinFolder']}"
       #cmd = "diff -r -I '^#.*' '/r02/helloworld' '/r02/helloworld_Modified'"
       diff_command_out = shell_out(cmd)
-      if diff_command_out.stdout == 0
-       #puts "#{diff['SourceFolder']} + "and" + #{diff['DestinFolder']} "are alike""
-          log 'same' do
-            message "#{diff['SourceFolder']} and  #{diff['DestinFolder']} are alike"
-            level :info
-          end
-       end
-      elseif diff_command_out.stdout ==1
-       #puts "#{diff['SourceFolder']} + "and" + #{diff['DestinFolder']} "are different""
-          log 'different' do
-            message "#{diff['SourceFolder']}" and "{diff['DestinFolder']}" "are different"
-            level :info
-          end
-       end
-      else
-       #puts "There was something wrong with the diff command"
-          log 'troubles' do
-            message "There was something wrong with the diff command"
-            level :info
-          end
-      end
+        if diff_command_out.stdout == "0"
+           puts "#{diff['SourceFolder']} and #{diff['DestinFolder']} are alike"
+          # log 'same' do
+          #   message "#{diff['SourceFolder']} and #{diff['DestinFolder']} are alike"
+          #   level :info
+          # end
+        elseif diff_command_out.stdout == "1"
+           puts "#{diff['SourceFolder']} and  #{diff['DestinFolder']} are different"
+          # log 'different' do
+          #   message "#{diff['SourceFolder']} and #{diff['DestinFolder']} are different"
+          #   level :info
+          # end
+        else
+           puts "There was something wrong with the diff command"
+          # log 'troubles' do
+          #   message "There was something wrong with the diff command"
+          #   level :info
+          # end
+        end
     end
-    action :run
+  action :run
 end
